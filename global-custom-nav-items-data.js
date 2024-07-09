a2a1043cccd86e2ac225b4175a8ce2268b534b07
alisha-gh <113233313+alisha-gh@users.noreply.github.com>
2024-07-09 12:48:37 +0930

    Create global-custom-nav-items-data.js

---
+++ global-custom-nav-items-data.js
@@ -0,0 +1,16 @@
+const globalCustomNav_items = [{
+    id: 1,
+    title: 'LTS',
+    icon_svg: 'icon-pin',
+    href: 'https://instructure.design/#icons-font',
+    target: '_blank',
+    position: 1, 
+  },
+  {
+    id: 2,
+    title: 'Hosted Icon',
+    icon_svg: 'https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/ui-icons/svg/Line/pin.svg',
+    href: 'https://community.canvaslms.com/',
+    target: '_blank',
+  }
+];
